7541f5036b8291cea072ed176e0fe807dd3b75c2
Melvin <melvin.levannhan@ynov.com>
2020-03-04 17:41:26 +0100

    First commit

---
+++ Outils.h
@@ -0,0 +1,12 @@
+void disp_menu();
+void disp_list();
+void Exit();
+void About();
+void Shop();
+void Save_game();
+void Loading();
+void lignes();
+void Continue();
+void New_game();
+void reveil();
+void Color(int t,int f);
---
+++ main.c
@@ -0,0 +1,5 @@
+
+void main()
+{
+    disp_menu();
+}
